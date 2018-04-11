3f0905e912fa0ce903fb12e3655cf6fb1887a193
Bradford Muron <bradfordmuron@ool-ad020304.dyn.optonline.net>
2018-04-11 14:16:21 -0400

    updated dep8

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ def updateTodo(item_id):
 
     # run Flask app in debug mode
 #app.run(debug=True)
-port = int(os.environ.get('PORT', 33507))
+port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
